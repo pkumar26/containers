7a4da42173a17e44951a3d7d8a1e756107187cd7
Pradeep Kumar <pradeep.kumar@microsoft.com>
2018-11-28 10:53:18 -0800

    updated server js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello, I am node!!!!\n');
+  res.send('Hello, Blackline!!!!\n');
 });
 
 app.listen(PORT, HOST);
